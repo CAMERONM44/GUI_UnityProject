9121013225a9631f5e05e0baaa1f2ce916c11bbc
CAMERONM44 <cameronmoltoni@gmail.com>
2025-05-05 16:46:56 +0800

    Created MainMenu Script and most of mobile view

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,16 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public GameObject optionsPanel;
+    public GameObject savesPanel;
+    public GameObject contactFormPanel;
+    public GameObject newGamePanel;
+    public Button buttonStart;
+    public Button buttonQuit;
+
+    void Awake()
     {
-        
+        optionsPanel.SetActive(false);
+        savesPanel.SetActive(false);
+        contactFormPanel.SetActive(false);
+        newGamePanel.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartGame()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(1);
+    }
+    public void ToggleOptionsPanel()
+    {
+        if (optionsPanel.activeSelf == true)
+        {
+            optionsPanel.SetActive(false);
+            buttonStart.enabled = true;
+            buttonQuit.enabled = true;
+        }
+        else
+        {
+            optionsPanel.SetActive(true);
+            buttonStart.enabled = false;
+            buttonQuit.enabled = false;
+        }
+    }
+    public void ToggleSavesPanel()
+    {
+        if (savesPanel.activeSelf == true)
+        {
+            savesPanel.SetActive(false);
+            buttonStart.enabled = true;
+            buttonQuit.enabled = true;
+        }
+        else
+        {
+            savesPanel.SetActive(true);
+            buttonStart.enabled = false;
+            buttonQuit.enabled = false;
+        }
+    }
+    public void ToggleContactFormPanel()
+    {
+        if (contactFormPanel.activeSelf == true)
+        {
+            contactFormPanel.SetActive(false);
+            buttonStart.enabled = true;
+            buttonQuit.enabled = true;
+        }
+        else
+        {
+            contactFormPanel.SetActive(true);
+            buttonStart.enabled = false;
+            buttonQuit.enabled = false;
+        }
+    }
+    public void ToggleNewGamePanel()
+    {
+        if (newGamePanel.activeSelf == true)
+        {
+            newGamePanel.SetActive(false);
+            buttonStart.enabled = true;
+            buttonQuit.enabled = true;
+        }
+        else
+        {
+            newGamePanel.SetActive(true);
+            buttonStart.enabled = false;
+            buttonQuit.enabled = false;
+        }
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
